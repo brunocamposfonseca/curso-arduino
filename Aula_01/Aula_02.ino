faf07ffcdcf5e8ba4c7de6c20a7e7458791fefc3
Bruno Campos Fonseca <92186023+brunocamposfonseca@users.noreply.github.com>
2024-02-23 07:46:34 -0300

    Update Aula_02.ino

--- Aula_01/Aula_02.ino
+++ Aula_01/Aula_02.ino
@@ -1,7 +1,12 @@
-void index{
-
+int led_1 = 13
+int led_2 = 12
+void setup(){
+  pinMode(led_1, OUTPUT)
+  pinMode(led_2, OUTPUT)
 }
 
-void loop{
-
+void loop(){
+  digitalWrite(led_1, HIGH)
+  digitalWrite(led_2, LOW)
+  
 }
